046847c512e9fdb42ee3f9c1d962e06eb3850738
Zafar Kamal <48084051+ZafarKamal123@users.noreply.github.com>
2023-09-05 18:35:40 +0500

    Add error handling

--- og-images/generate.js
+++ og-images/generate.js
@@ -5,143 +5,167 @@ const axios = require("axios");
 const { get } = require("lodash");
 const { parse } = require("node-html-parser");
 
-const OG_IMAGES_DIRECTORY_PATH = './og-images/images';
+const OG_IMAGES_DIRECTORY_PATH = "./og-images/images";
 
-const template = path.resolve(__dirname, 'templates/index.html');
+const template = path.resolve(__dirname, "templates/index.html");
 
 const getThemes = () => {
-	const absoluteSource = './themes';
+  const absoluteSource = "./themes";
 
-	return fs
-		.readdirSync( absoluteSource, { withFileTypes: true } )
-		.filter( ( dirent ) => dirent.isDirectory() )
-		.map( ( dirent ) => dirent.name );
+  return fs
+    .readdirSync(absoluteSource, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name);
 };
 
 const getThemeStyles = async (slug) => {
-    
-    try {
-        let indexPath = path.resolve(__dirname, `../themes/${slug}/index.html`);
-        
-        if (fs.existsSync(indexPath)) {
-            let themeDemo = fs.readFileSync(path.resolve(__dirname, `../themes/${slug}/index.html`), "utf8");
-            let parsedStyles = parse(themeDemo).querySelectorAll('link, style').map(styleElem => styleElem.outerHTML).join('');
-            return parsedStyles;
-        }
+  try {
+    let indexPath = path.resolve(__dirname, `../themes/${slug}/index.html`);
 
-
-    } catch (err) {
-        console.log('Template index not found for: ', slug);
-        return [];
+    if (fs.existsSync(indexPath)) {
+      let themeDemo = fs.readFileSync(
+        path.resolve(__dirname, `../themes/${slug}/index.html`),
+        "utf8"
+      );
+      let parsedStyles = parse(themeDemo)
+        .querySelectorAll("link, style")
+        .map((styleElem) => styleElem.outerHTML)
+        .join("");
+      return parsedStyles;
     }
+  } catch (err) {
+    console.log("Template index not found for: ", slug);
+    return [];
+  }
+};
 
-}
+async function updateTemplateWithDetails(page, themeDetails) {
+  try {
+    const { name, screenshot_url, description, slug } = themeDetails;
 
-async function updateTemplateWithDetails( page, themeDetails ) {
-
-    const {
-        name,
-        screenshot_url,
-        description,
-        slug
-    } = themeDetails;
-    
     const themeJSONPath = `../themes/${slug}/theme.json`;
 
-    if (! fs.existsSync(path.resolve(__dirname, themeJSONPath)) ) {
-        console.log('🚫 Theme json not found for: ', slug);
-        
-        
-        const leftoversJSONPath = path.resolve(__dirname, `../data/leftovers.json`);
+    if (!fs.existsSync(path.resolve(__dirname, themeJSONPath))) {
+      console.log("🚫 Theme json not found for: ", slug);
 
-        const leftoversJSON = require(leftoversJSONPath);
-        leftoversJSON.push(slug);
-        fs.writeFileSync(leftoversJSONPath, JSON.stringify(leftoversJSON, null, 2));
-        return;
+      const leftoversJSONPath = path.resolve(
+        __dirname,
+        `../data/leftovers.json`
+      );
+
+      const leftoversJSON = require(leftoversJSONPath);
+      leftoversJSON.push(slug);
+      fs.writeFileSync(
+        leftoversJSONPath,
+        JSON.stringify(leftoversJSON, null, 2)
+      );
+      return;
     }
 
     const themeJSON = require(themeJSONPath);
 
     const themeStyles = await getThemeStyles(slug);
 
-    const colorPalette = get(themeJSON, 'settings.color.palette', []).map(color => {
-        return get(color, 'color');
-    }).filter(color => typeof color !== 'undefined').slice(0, 5);
+    const colorPalette = get(themeJSON, "settings.color.palette", [])
+      .map((color) => {
+        return get(color, "color");
+      })
+      .filter((color) => typeof color !== "undefined")
+      .slice(0, 5);
 
-    const response = await axios.get(
-        'http:'  + screenshot_url,
-        {
-            responseType: 'arraybuffer'
-        }
-    );
+    const response = await axios.get("http:" + screenshot_url, {
+      responseType: "arraybuffer",
+    });
     const buffer = Buffer.from(response.data);
-    const base64encodedImage = buffer.toString('base64');
+    const base64encodedImage = buffer.toString("base64");
 
     // Adding theme styles
     await page.evaluate((styles) => {
+      document.querySelector("head").innerHTML += styles;
+    }, themeStyles);
 
-        document.querySelector('head').innerHTML += styles;
+    await page.evaluate(
+      (name, description, screenshot, pallete) => {
+        let shortDescription =
+          description.length > 100
+            ? description.slice(0, 97).concat("...")
+            : description;
 
-    }, themeStyles)
+        document.body.innerHTML = document.body.innerHTML.replace(
+          /{{theme_title}}/g,
+          name
+        );
+        document.body.innerHTML = document.body.innerHTML.replace(
+          /{{theme_description}}/g,
+          shortDescription
+        );
+        document.body.innerHTML = document.body.innerHTML.replace(
+          /{{theme_screenshot}}/g,
+          "data:image/png;base64," + screenshot
+        );
 
-
-    await page.evaluate((name, description, screenshot, pallete) => {
-
-        let shortDescription = description.length > 100 ? description.slice(0, 97).concat('...') : description;
-
-        document.body.innerHTML = document.body.innerHTML.replace(/{{theme_title}}/g, name);
-        document.body.innerHTML = document.body.innerHTML.replace(/{{theme_description}}/g, shortDescription);
-        document.body.innerHTML = document.body.innerHTML.replace(/{{theme_screenshot}}/g, 'data:image/png;base64,' + screenshot);
-        
         if (pallete.length === 0) {
-            document.querySelector('.home-colorscheme731').style.border = '';
+          document.querySelector(".home-colorscheme731").style.border = "";
         }
 
-        document.querySelector('#color-pallete-root').innerHTML = pallete.map(color => {
-            return `<li class="pallete" style="--color:${color};"></li>`
-        }).join('\n')
+        document.querySelector("#color-pallete-root").innerHTML = pallete
+          .map((color) => {
+            return `<li class="pallete" style="--color:${color};"></li>`;
+          })
+          .join("\n");
+      },
+      name,
+      description,
+      base64encodedImage,
+      colorPalette
+    );
+  } catch (error) {
+    console.error("🛑 Something went wrong... Skipping");
 
-    }, name, description, base64encodedImage, colorPalette);
+    const leftoversJSONPath = path.resolve(__dirname, `../data/leftovers.json`);
 
+    const leftoversJSON = require(leftoversJSONPath);
+    leftoversJSON.push(themeDetails.slug);
+    fs.writeFileSync(leftoversJSONPath, JSON.stringify(leftoversJSON, null, 2));
+
+    return false;
+  }
 }
 
 async function main() {
-    const browser = await puppeteer.launch({
-        headless: true,
-        'args': [
-            '--no-sandbox'
-        ],
-    });
-    
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox"],
+  });
 
-    const themes = getThemes();
+  let themes = getThemes();
 
-    for (const theme of themes) {
+  for (const theme of themes) {
+    const page = await browser.newPage();
 
-        console.log('😎 On theme: ' + theme);
+    const themeDetails = require(`../themes/${theme}/details.json`);
 
-        const page = await browser.newPage();
+    await page.goto(`file:${template}`, { waitUntil: "networkidle2" });
 
-        const themeDetails = require(`../themes/${theme}/details.json`);
+    await page.waitForSelector("#img");
 
-        await page.goto(`file:${template}`, { waitUntil: 'networkidle2' });
+    const isUpdated = await updateTemplateWithDetails(page, themeDetails);
 
-        await page.waitForSelector('#img');
+    if (isUpdated) {
+      const target = await page.$(".home-container");
+      const boundingBox = await target.boundingBox();
 
-        await updateTemplateWithDetails(page, themeDetails);
-
-        const target = await page.$('.home-container');
-        const boundingBox = await target.boundingBox();
-
-        await page.setViewport({ width: 1200, height: boundingBox.height });
-        await target.screenshot({ path: path.resolve(OG_IMAGES_DIRECTORY_PATH,  theme + '.jpg') });
-
-        await page.close()
+      await page.setViewport({ width: 1200, height: boundingBox.height });
+      await target.screenshot({
+        path: path.resolve(OG_IMAGES_DIRECTORY_PATH, theme + ".jpg"),
+      });
     }
 
-    console.log('🎉 Og image generation completed');
-    await browser.close();
+    await page.close();
+  }
 
+  console.log("🎉 Og image generation completed");
+  await browser.close();
 }
 
 main();
